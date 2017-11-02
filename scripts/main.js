44a5e52940614512f42b5c452b7e80c644973c57
core2for <basicscoreformy@outlook.com>
2017-11-02 15:14:09 +0800

    fix the bug of image

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/dog.jpg') {
+	if(mySrc === '../images/dog.jpg') {
 		myImage.setAttribute('src', '../images/dog2.jpg');
 	} else {
 		myImage.setAttribute('src', '../images/dog.jpg');
